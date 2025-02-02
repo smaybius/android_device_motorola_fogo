9ccf8be4ffea2c8ec687eb0026f8001e7b08e02a
Connor Mitchell <37678351+smaybius@users.noreply.github.com>
2025-02-02 12:59:46 -0500

    Source: device_motorola_rtwo

--- extract-files.py
+++ extract-files.py
@@ -4,28 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.file import File
+from extract_utils.extract import extract_fns_user_type
+from extract_utils.extract_star import extract_star_firmware
 from extract_utils.fixups_blob import (
-    BlobFixupCtx,
     blob_fixup,
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
-    lib_fixup_remove_arch_suffix,
-    lib_fixup_vendorcompat,
 )
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
-)
-from extract_utils.tools import (
-    llvm_objdump_path,
-)
-from extract_utils.utils import (
-    run_cmd,
 )
 
 namespace_imports = [
@@ -35,48 +26,19 @@ namespace_imports = [
     "vendor/qcom/opensource/commonsys/display",
     "vendor/qcom/opensource/commonsys-intf/display",
     "vendor/qcom/opensource/dataservices",
+    'vendor/qcom/opensource/display',
 ]
 
-
-def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}_{partition}' if partition == 'vendor' else None
-
-
 lib_fixups: lib_fixups_user_type = {
-    'libs_clang_rt_ubsan': lib_fixup_remove_arch_suffix,
-    'libs_proto_3_9_1': lib_fixup_vendorcompat,
-    'libwpa_client': lib_fixup_remove,
+    **lib_fixups,
 }
-
-
-def blob_fixup_return_1(
-    ctx: BlobFixupCtx,
-    file: File,
-    file_path: str,
-    symbol: str,
-    *args,
-    **kwargs,
-):
-    for line in run_cmd(
-        [
-            llvm_objdump_path,
-            '--dynamic-syms',
-            file_path,
-        ]
-    ).splitlines():
-        if line.endswith(f' {symbol}'):
-            offset, _ = line.split(maxsplit=1)
-
-            with open(file_path, 'rb+') as f:
-                f.seek(int(offset, 16))
-                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
-                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
-
-            break
-
 
 blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
+
+extract_fns: extract_fns_user_type = {
+    r'(bootloader|radio)\.img': extract_star_firmware,
+}
 
 module = ExtractUtilsModule(
     'fogo',
@@ -84,6 +46,9 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    extract_fns=extract_fns,
+    add_firmware_proprietary_file=True,
+    add_generated_carriersettings=True,
 )
 
 if __name__ == '__main__':
