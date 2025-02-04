32ca882c6b6ecf306578743e8c3f1bc3e0183481
Connor Mitchell <37678351+smaybius@users.noreply.github.com>
2025-02-04 11:41:22 -0500

    libmdmcutback is said to need to be removed

--- extract-files.py
+++ extract-files.py
@@ -27,7 +27,7 @@ namespace_imports = [
     "vendor/qcom/opensource/commonsys-intf/display",
     "vendor/qcom/opensource/dataservices",
     'vendor/qcom/opensource/display',
-    "hardware/qcom/wlan/wcn6740",
+    "hardware/qcom/wlan/wcn6740"
 ]
 
 
