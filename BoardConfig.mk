9529d18354da07e17e5f7ff5ddaf2c242285042e
Connor Mitchell <37678351+smaybius@users.noreply.github.com>
2025-02-06 11:14:59 -0500

    make: *** kernel/motorola/fogo: No such file or directory.  Stop.
    Revert "make: *** kernel/motorola/fogo: No such file or directory.  Stop."
    
    This reverts commit f33799f99f3e028de0971725a38142327b20113f.
    
    Found the kernel source: https://github.com/MotorolaMobilityLLC/kernel-msm/tree/android-14-release-u1ufn34.41-70r3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_CONFIG := fogo_defconfig
+TARGET_KERNEL_CONFIG := gki_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/fogo
 
 # Kernel - prebuilt
